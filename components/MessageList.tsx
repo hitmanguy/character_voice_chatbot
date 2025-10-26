634d0f001425d6d587429eb0a2c50486c9a0ce69
hitmanguy <csahil1606@gmail.com>
2025-10-26 11:20:03 +0530

    final

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { AnimatePresence } from 'framer-motion';
-import { Message } from '../types';
-import { useAutoScroll } from '../hooks/useAutoScroll';
-import { MessageBubble } from './MessageBubble';
-import { TypingIndicator } from './TypingIndicator';
+import React from "react";
+import { AnimatePresence } from "framer-motion";
+import { Message } from "../types";
+import { useAutoScroll } from "../hooks/useAutoScroll";
+import { MessageBubble } from "./MessageBubble";
+import { TypingIndicator } from "./TypingIndicator";
 
 interface MessageListProps {
   messages: Message[];
@@ -11,17 +11,28 @@ interface MessageListProps {
   isBotSpeaking: boolean;
 }
 
-export const MessageList: React.FC<MessageListProps> = ({ messages, isTyping, isBotSpeaking }) => {
+export const MessageList: React.FC<MessageListProps> = ({
+  messages,
+  isTyping,
+  isBotSpeaking,
+}) => {
   const scrollRef = useAutoScroll([messages, isTyping]);
 
   return (
-    <div ref={scrollRef} className="flex-1 overflow-y-auto p-6 space-y-4 pt-24 pb-48">
+    <div
+      ref={scrollRef}
+      className="flex-1 overflow-y-auto p-6 space-y-4 pt-24 pb-48"
+    >
       <AnimatePresence>
         {messages.map((msg, index) => (
-          <MessageBubble 
-            key={msg.id} 
+          <MessageBubble
+            key={msg.id}
             message={msg}
-            isSpeaking={msg.sender === 'bot' && index === messages.length -1 && isBotSpeaking}
+            isSpeaking={
+              msg.sender === "bot" &&
+              index === messages.length - 1 &&
+              isBotSpeaking
+            }
           />
         ))}
       </AnimatePresence>
